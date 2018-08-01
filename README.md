3df274795edb74fb6495e8dd03a9d2c5cb001e42
MasonCaiby <maxwell.caudle@gmail.com>
2018-08-01 11:12:13 -0600

    just need to add doc strings and comments and things

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project is an attempt to streamline / automate the job search process. I am automating applying to job on [Angel](https://angel.co/). I'm going to use Selenium for this, it will open a page in the background and navigate through it. This is going to be a 2 part README, partially so you can get it working, and partially so you can see how the code works - which I guess is the point of all READMEs.
 
+## Setup
+
 First let's install [Selenium](https://www.seleniumhq.org/):
 1. `pip install selenium`
 2. Navigate to [this page](http://selenium-python.readthedocs.io/installation.html#drivers) and select the browser you want to use. I chose Chrome, so Google can get more of my data. You'll then download the correct folder (unzip it if needed) and move it to your path. Mine was in my `../Downloads/` folder, and I moved it to `usr/local/bin`: just copy/paste this: `sudo mv chromedriver /usr/local/bin/`
@@ -12,4 +14,14 @@ to a .csv as `email@email.com,password`. If you end up doing any git related act
 You'll also (currently) need to modify the `html.txt` file to whatever it is when
 you select the relevant filter you want in the [jobs page](https://angel.co/jobs). I've uploaded the one I'm using as an example. If you live in Boulder, CO are fine working remotely and want a job as a Data Scientist that is: Full-Time, Contract, or an Internship; you don't need to change this.
 
-Now change the `spiel.txt` file to whatever you want your spiel to be. Keep it around 950 characters, so we can add the recruiter's name to our little cover letter guy. Mine is also included in this repo, as an example. If you feel like trying to get me a job, you can use my cover letter.
+Now change the `spiel_auto.txt` file and `spiel_manual.txt` to whatever you want your spiels to be. It alternates between the two cover letters, for AB testing. Data Science. Keep it around 950 characters, so we can add the recruiter's name to our little cover letter guy. Mine is also included in this repo, as an example. If you feel like trying to get me a job, you can use my cover letter.
+
+If you want, you can watch a video of it chugging away: [here](https://vimeo.com/282698022).
+
+## What it does
+
+So this script uses Selenium. It first opens a new driver instance (called browser)and navigates to the Angel List login page. It then uses the credentials it pulled from your .csv file and logins into the page. Now it navigates the the URL you saved in your html.txt file. The url should have all relevant filters BE SELECTIVE- Angel list has a limit on the number of open job apps you can have. Then it steps through all `_jm` classes and check to see if it has 'applicants last week' in its text. If so, it clicks on the `<div>` to expand it, clicks on Apply Now, grabs the recruiters name, and puts a message in the `<textarea>` box. Then it clicks Submit or Send or Apply or whatever and moves on to the next one.
+
+Easily the hardest part of all this was figuring out the page structure and how Selenium interacted with it - I've never used Selenium before and I do more Data Science than Web Development. But I learned a lot and I think it works pretty well.
+
+I hope you don't find yourself on the job market anytime soon, but if you do, maybe you can use this to help with your search. I'd recommend putting it in a [schedule](https://github.com/dbader/schedule) function. 
--- main.py
+++ main.py
@@ -3,103 +3,164 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
-# get login creds
-angel_creds = np.genfromtxt('angel_creds.csv', delimiter=',', dtype=str)
-angel_email_cred = angel_creds[0]
-angel_password_cred = angel_creds[1]
 
-# create browser object, navigate to login
-browser = webdriver.Chrome()
-browser.get('https://angel.co/login')
+def get_files():
+    # get the spiel_auto text
+    with open('spiel_auto.txt', 'r') as spiel_file:
+        spiel_auto = spiel_file.read()
 
-# try to login, if can't, I think I'll be pre-logged in
-angel_email_input = browser.find_element_by_id('user_email')
-angel_email_input.send_keys(angel_email_cred)
-angel_password_input = browser.find_element_by_id('user_password')
-angel_password_input.send_keys(angel_password_cred)
-angel_password_input.submit()
+    # get the spiel_manual text
+    with open('spiel_manual.txt', 'r') as spiel_file:
+        spiel_manual = spiel_file.read()
 
-# navigate to the jobs page this loads the relevant filters
-with open('html.txt', 'r') as html_file:
-    filtered_html = html_file.read()
-browser.get(filtered_html)
+    # get the filtered URL
+    with open('html.txt', 'r') as html_file:
+        filtered_html = html_file.read()
+
+    return spiel_auto, spiel_manual, filtered_html
+
+
+def get_creds():
+    # get login creds
+    angel_creds = np.genfromtxt('angel_creds.csv', delimiter=',', dtype=str)
+    angel_email_cred = angel_creds[0]
+    angel_password_cred = angel_creds[1]
+
+    return angel_email_cred, angel_password_cred
+
+
+def get_broswer_and_login(angel_email_cred, angel_password_cred,
+                          filtered_html):
+    # create browser object, navigate to login
+    browser = webdriver.Chrome()
+    browser.get('https://angel.co/login')
+
+    # try to login, if can't, I think I'll be pre-logged in
+    try:
+        angel_email_input = browser.find_element_by_id('user_email')
+        angel_email_input.send_keys(angel_email_cred)
+        angel_password_input = browser.find_element_by_id('user_password')
+        angel_password_input.send_keys(angel_password_cred)
+        angel_password_input.submit()
+    except Exception as e:
+        print(e)
+        pass
+
+    # navigate to the jobs page this loads the relevant filters
+    browser.get(filtered_html)
+
+    return browser
+
 
 # #TODO: open the filters drop down and select the filters I want.
 """select relevant filters. Angel uses and/or filters it appears, so
 I'm not worried about pre selected filters. Since I'm fine with post
 match filtering I'll leave any pre-loaded ones on"""
 
-# Time to step through the different job postings and click apply
-time.sleep(5) # w/o sleep get a stale element reference error
 
-# scroll to bottom of page | Load full page
-SCROLL_PAUSE_TIME = 1
-# Get scroll height
-last_height = browser.execute_script("return document.body.scrollHeight")
-while True:
-    print('Scrolling...')
-    # Scroll down to bottom
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
-    # Calculate new scroll height and compare with last scroll height
-    new_height = browser.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+def scroll_through_page(browser):
+    # scroll to bottom of page | Load full page
+    SCROLL_PAUSE_TIME = 1
+    # Get scroll height
+    last_height = browser.execute_script("return document.body.scrollHeight")
+    while True:
+        print('Scrolling...')
+        # Scroll down to bottom
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+        # Calculate new scroll height and compare with last scroll height
+        new_height = browser.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
 
-#get the correct elements
-jms = browser.find_elements_by_class_name('_jm')
-print("number of jm elements: ", len(jms))
 
-number_of_jobs = 0
-for jm in jms[:1]:
-    # this is needed to not navigate to a new window
-    if 'applicants last week' in jm.text:
-        jm.click()
-        time.sleep(0.5)
-        apply_now = jm.find_element_by_class_name('apply-now-button')
-        if apply_now.text == 'Apply Now':
-            number_of_jobs += 1
-        company_name = jm.find_element_by_class_name('startup-link').text
-        print(apply_now.text + ' | ' + company_name + ' | ' + str(number_of_jobs))
-        time.sleep(0.5)
+def apply_to_job(apply_now, spiel, browser):
+    # open application window
+    apply_now.click()
+    time.sleep(3)
+
+    # get recruiter's name
+    recruiting_contact = browser.find_element_by_class_name('recruiting-contact')
+    recruiter_name = recruiting_contact.find_element_by_class_name('name').text
+    recruiter_name_list = recruiter_name.split(' ')
+    if len(recruiter_name_list) > 1:
+        recruiter_name = recruiter_name_list[0]
+
+    new_spiel = recruiter_name + ',\n\n' + spiel
+
+    text_box_div = browser.find_element_by_class_name('_1xXo9j7wJhYoCN1vk_CNsT')
+    text_box = text_box_div.find_element_by_tag_name('textarea')
+
+    text_box.click()
+    text_box.send_keys(new_spiel)
+
+    submit_button = text_box_div.find_element_by_class_name('c-button--blue')
+    submit_button.click()
+
+    time.sleep(2)
+    close_section = browser.find_element_by_class_name('_3Aslx7L3GVI4XM7PUyYKza')
+    close_button = close_section.find_element_by_class_name('c-button--blue')
+    close_button.click()
+    time.sleep(2)
+    return recruiter_name
+
+
+def step_through_jobs(browser, spiel_auto, spiel_manual):
+    # get the correct elements
+    jms = browser.find_elements_by_class_name('_jm')
+    print("number of jm elements: ", len(jms))
+
+    number_of_jobs = 1
+
+    for jm in jms:
+        # this is needed to not navigate to a new window
+        if 'applicants last week' in jm.text:
+            if number_of_jobs % 2:
+                spiel = spiel_auto
+            else:
+                spiel = spiel_manual
+            jm.click()
+            company_name = jm.find_element_by_class_name('startup-link').text
+            time.sleep(3)
+            apply_now = jm.find_element_by_class_name('apply-now-button')
+            if apply_now.text == 'Apply Now':
+                try:
+                    recruiter_name = apply_to_job(apply_now, spiel, browser)
+                except:
+                    recruiter_name = "Dear Hiring Manager,"
+                print(company_name + ' | ' + str(number_of_jobs) + ' | ' +
+                      recruiter_name)
+                time.sleep(2)
+                number_of_jobs += 1
+
+    print(number_of_jobs)
+
+
+
 # After looking at this some, and seeing that it fails on some job postings,
-# It seems like there is an issue with some job postings "attaching" themselves
-# to a previous posting. It seems like they wrap a random number if jobs in a div
-# this could be a scrolling issue? For now, I'm going to skip these
+# It seems like there is an issue with some job postings "attaching"
+# themselves to a previous posting. It seems like they wrap a random number if
+# jobs in a div this could be a scrolling issue? For now, I'm going to skip
+# these
 # #TODO: investigate more.
-print(number_of_jobs)
-
-# open application window
-apply_now.click()
-time.sleep(3)
-
-# get recruiter's name
-recruiting_contact = browser.find_element_by_class_name('recruiting-contact')
-recruiter_name = recruiting_contact.find_element_by_class_name('name').text
-print('recruiter_name = ', recruiter_name)
-recruiter_name_list = recruiter_name.split(' ')
-if len(recruiter_name_list) > 1:
-    recruiter_name = recruiter_name_list[0]
 
 
-with open('spiel.txt', 'r') as spiel_file:
-    spiel = spiel_file.read()
+def main():
+        spiel_auto, spiel_manual, filtered_html = get_files()
+        angel_email_cred, angel_password_cred = get_creds()
 
-new_spiel = recruiter_name + ',\n\n' + spiel
+        browser = get_broswer_and_login(angel_email_cred, angel_password_cred,
+                                        filtered_html)
 
-text_box_div = browser.find_element_by_class_name('_1xXo9j7wJhYoCN1vk_CNsT')
-text_box = text_box_div.find_element_by_tag_name('textarea')
-print(text_box.is_displayed())
+        # Time to step through the different job postings and click apply
+        time.sleep(5)  # w/o sleep get a stale element reference error
 
-text_box.click()
-text_box.send_keys(new_spiel)
+        scroll_through_page(browser)
 
-submit_button = text_box_div.find_element_by_class_name('c-button--blue')
-submit_button.click()
+        step_through_jobs(browser, spiel_auto, spiel_manual)
 
-time.sleep(2)
-close_section = browser.find_element_by_class_name('_3Aslx7L3GVI4XM7PUyYKza')
-close_button = close_section.find_element_by_class_name('c-button--blue')
-close_button.click()
+
+if __name__ == '__main__':
+    main()
